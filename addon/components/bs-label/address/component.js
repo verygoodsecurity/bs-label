1aa4a32aab3473587c3770b8e60e609950075cd0
cohitre <carlosrr@gmail.com>
2016-12-12 17:24:30 -0500

    Adding bs-label/address

---
+++ addon/components/bs-label/address/component.js
@@ -0,0 +1,29 @@
+import EmComponent from 'ember-component';
+import layout from './template';
+import computed, { reads } from 'ember-computed';
+import { A } from 'ember-array/utils';
+import { isBlank } from 'ember-utils';
+
+export default EmComponent.extend({
+  layout,
+  tagName: 'address',
+  classNameBindings: ['presenter.classNames'],
+
+  line1: reads('presenter.address.line1'),
+  line2: reads('presenter.address.line2'),
+  line3: computed('presenter.address.city', 'presenter.address.region', 'presenter.address.postalCode', function() {
+    const city = this.get('presenter.address.city');
+    const region = this.get('presenter.address.region');
+    const postalCode = this.get('presenter.address.postalCode');
+    return A([city, region, postalCode]).reject(isBlank).join(', ');
+  }),
+  addressLines: computed('line1', 'line2', 'line3', 'presenter.address.country', function() {
+    return A([
+      this.get('line1'),
+      this.get('line2'),
+      this.get('line3'),
+      this.get('presenter.address.country'),
+    ])
+    .reject(isBlank);
+  }),
+});
---
+++ tests/integration/components/bs-label/address-test.js
@@ -0,0 +1,64 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('bs-label/address', 'Integration | Component | bs-label/address', {
+  integration: true
+});
+
+test('it adds the classNames', function(assert) {
+  this.set('configuration', { classNames: 'danger' });
+  this.render(hbs`{{bs-label/address presenter=configuration}}`);
+  assert.equal(this.$('address').is('.danger'), true);
+  assert.deepEqual(this.$('address').text(), '');
+});
+
+test('it renders the address', function(assert) {
+  const self = this;
+  function t(address, expectation) {
+    self.set('configuration', { address });
+    self.render(hbs`{{bs-label/address presenter=configuration}}`);
+    const result = self.$('address').text().split('\n').map(a => a.trim());
+    assert.deepEqual(result, expectation);
+  }
+  t({
+    line1: '123 Main St',
+    line2: '#200',
+    city: 'Metropolis',
+    region: 'MM',
+    postalCode: '00000',
+    country: 'USA'
+  }, [
+    "123 Main St",
+    "#200",
+    "Metropolis, MM, 00000",
+    "USA",
+    ""
+  ]);
+
+  t({
+    line1: '123 Main St',
+    city: 'Metropolis',
+    region: 'MM',
+    postalCode: '00000',
+    country: 'USA'
+  }, [
+    "123 Main St",
+    "Metropolis, MM, 00000",
+    "USA",
+    ""
+  ]);
+
+  t({
+    line1: '123 Main St',
+    line2: '#200',
+    region: 'MM',
+    postalCode: '00000',
+    country: 'USA'
+  }, [
+    "123 Main St",
+    "#200",
+    "MM, 00000",
+    "USA",
+    ""
+  ]);
+});
