ffa320f6460711c6ef2a5fbe7982754d99985677
cohitre <carlosrr@gmail.com>
2016-12-29 12:05:08 -0800

    Adding target binding

--- tests/integration/components/bs-label/link-to-test.js
+++ tests/integration/components/bs-label/link-to-test.js
@@ -14,7 +14,23 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{bs-label/link-to presenter=presenter}}`);
   const link = this.$().find('a');
+  assert.equal(link.text(), 'cool link 123');
+  assert.equal(link.is('.cool-link'), true);
+  assert.equal(link.attr('target'), undefined);
+});
+
+test('it adds the target', function(assert) {
+  this.set('presenter', {
+    text: 'cool link 123',
+    classNames: 'cool-link',
+    target: '_blank',
+    params: ['administrator.tools', { id: 123 }],
+  });
+
+  this.render(hbs`{{bs-label/link-to presenter=presenter}}`);
+  const link = this.$().find('a');
 
   assert.equal(link.text(), 'cool link 123');
   assert.equal(link.is('.cool-link'), true);
+  assert.equal(link.attr('target'), '_blank');
 });
